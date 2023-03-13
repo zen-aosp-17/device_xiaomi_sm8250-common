957ec4497dcd3eca4c01ea34734ce2d91c98430a
Wei Wang <wvw@google.com>
2023-03-14 03:03:35 +0600

    sm8250-common: Boot time tuning
    Test: build and boottime test
    Change-Id: Ib8f31d39501b4747f0c268d2dd87e814bcc7843b
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: Kshitij Koyande <68855313+Roxor-007@users.noreply.github.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -6129,6 +6129,20 @@ case "$target" in
         ;;
 esac
 
+# Setup runtime schedTune
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+echo "schedutil" > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
+echo 1 > /dev/stune/foreground/schedtune.prefer_idle
+echo 0 > /dev/stune/foreground/schedtune.prefer_high_cap
+echo 0 > /dev/stune/foreground/schedtune.boost
+echo 0 > /dev/stune/schedtune.prefer_idle
+echo 0 > /dev/stune/schedtune.prefer_high_cap
+echo 0 > /dev/stune/schedtune.boost
+echo 1 > /dev/stune/top-app/schedtune.prefer_idle
+echo 0 > /dev/stune/top-app/schedtune.prefer_high_cap
+echo 10 > /dev/stune/top-app/schedtune.boost 10
+
 # Post-setup services
 case "$target" in
     "msm8660" | "msm8960" | "msm8226" | "msm8610" | "mpq8092" )

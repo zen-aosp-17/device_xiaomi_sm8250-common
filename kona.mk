f905759c701f206970672905ebdf76acd3765528
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-04 13:52:38 +0800

    sm8250-common: device: Build AOSP Radio HIDL->AIDL Wrapper
    Depends-On: I790728a9be289042e5a07589934a2dd50463a507
    Change-Id: Iaeb8d37f4a7f02e6e812bc4da1b413c9a6493f8f

--- kona.mk
+++ kona.mk
@@ -378,6 +378,7 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Telephony
 PRODUCT_PACKAGES += \
+    android.hardware.radio-service.compat \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \

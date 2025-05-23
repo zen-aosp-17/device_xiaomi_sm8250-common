21b9efd97ca845367b770bcc25d59dc63e3a696c
Bruno Martins <bgcngm@gmail.com>
2025-05-23 23:08:14 +0800

    sm8250-common: device: Set camera override format from reserved via soong config
    Change-Id: I6a089e783f26c4515940d74f8e912dffa2789550

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,10 +77,6 @@ MALLOC_SVELTE_FOR_LIBC32 := true
 
 # Camera - Miui
 TARGET_CAMERA_PACKAGE_NAME := com.android.camera
-# Camera - HIDL overrideFormat
-TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT = true
-# Camera - HIDL overrideFormat - Alternative Version
-TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED = true
 # Camera - Torch
 TARGET_CAMERA_SERVICE_EXT_LIB := //$(COMMON_PATH):libcameraservice_extension.xiaomi_sm8250
 # Display
--- kona.mk
+++ kona.mk
@@ -136,6 +136,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
+$(call soong_config_set,camera,override_format_from_reserved,true)
+
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \

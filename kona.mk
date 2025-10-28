ff51dd6049cfbfbfb0aac8842ac9e4d0a2770354
yaseakun <agathagemink2014@gmail.com>
2025-10-28 07:17:12 +0000

    sm8250-common: Set soong names  for Miui/Leica Camera

--- kona.mk
+++ kona.mk
@@ -137,6 +137,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64
 
 $(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set,camera,package_name,com.android.camera)
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \

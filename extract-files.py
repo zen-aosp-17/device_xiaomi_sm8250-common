dd5eeac4d0f9bc6ed3c9716c5b98bac47fb28c0b
ralph950412 <ralph950412@gmail.com>
2026-06-18 04:16:44 +0800

    sm8250-common: Patch libwfdcommonutils to remove its libheif dependency
    Due to https://android.googlesource.com/platform/frameworks/av/+/baa3574157c3a90e721da7460b65bb8c6d130158
    
    Change-Id: Ie0105c391ae187b79ec5f5beb150dc786df95d1b
    Signed-off-by: n0escape-exe <viaan4542@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -18,6 +18,8 @@ from extract_utils.fixups_lib import (
 )
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libwfdcommonutils.so': blob_fixup()
+        .remove_needed('libheif.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libaudiobase.so')
         .add_needed('libgui_shim.so'),

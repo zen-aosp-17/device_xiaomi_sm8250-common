ef1ca728d4399de952cf4aa0633c41e6493cf8fc
binbin323 <bin@dhao.cc>
2026-06-30 03:15:50 +0000

    sm8250-common: Patch wfdservice with libaudiobase Change-Id: I8d018184154137f55c8a486b64638225b40db2d1
    Signed-off-by: n0escape-exe <viaan4542@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -24,6 +24,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .add_needed('libaudiobase.so')
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
         .regex_replace('.*seclabel u:r:mi_thermald:s0\n', ''),

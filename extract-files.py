4c012fad9a6cfce1d35442b209bfae90085dbcb4
binbin323 <bin@dhao.cc>
2026-06-30 03:12:27 +0000

    sm8250-common: Patch libwfdmmsrc_system with libgui_shim
    Change-Id: Ieac7d379639baf1127a469a75743ecd723d16d33
    Signed-off-by: n0escape-exe <viaan4542@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -18,6 +18,8 @@ from extract_utils.fixups_lib import (
 )
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()

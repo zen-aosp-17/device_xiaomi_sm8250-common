ed9b6d15a71c6c0c62fb30231bbdd9cf02132b0a
zenzer0s <praveenzalaki200@gmail.com>
2026-08-05 22:03:31 +0530

    [SQUASH] Revert "sm8250-common: Import XiaomiParts from sdm845-common"
    moved to device/xiaomi/parts

--- kona.mk
+++ kona.mk
@@ -157,10 +157,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 endif
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service
